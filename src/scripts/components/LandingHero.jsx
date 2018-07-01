56c641c8ad9c0bffe46695fbba266f51daf61906
Robert Pratt <bpong@v8ch.com>
2018-07-01 17:03:15 +0700

    Shift to React component via AirBNB style

--- src/scripts/components/LandingHero.jsx
+++ src/scripts/components/LandingHero.jsx
@@ -1,36 +1,32 @@
-import PropTypes from 'prop-types';
+import React from 'react'; // eslint-disable-line import/no-unresolved
 import V8chLogo from './V8chLogo';
 
-const { Component } = wp.element;
-
-class LandingHero extends Component {
-    constructor() {
-        super(...arguments);
-        console.log(`[LandingHero] constructor() this.props: ${JSON.stringify(this.props)}`);
-    }
-    render() {        
-        return (
-            <div className="frame frame--dark-background frame--hero type--v8ch">
-                <div className="frame__container">
-                    <div className="dual-header dual-header--light">
-                        <h3>Robert Pratt</h3>
-                        <h5>Full-stack developer</h5>
-                    </div>
-                </div>
-                <div className="frame__container frame__container--center">
-                    <V8chLogo size={this.props.logoSize}/>
-                </div>
-                <div className="frame__container frame__container--bottom">
-                    <div className="split-header split-header--light">
-                        <h4>Favor creation</h4>
-                        <h5>Over consumption</h5>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
-LandingHero.propTypes = {logoSize: PropTypes.string};
+const LandingHero = () => (
+  <div className="frame frame--dark-background frame--hero type--v8ch">
+    <div className="frame__container">
+      <div className="dual-header dual-header--light">
+        <h3>
+          Robert Pratt
+        </h3>
+        <h5>
+          Full-stack developer
+        </h5>
+      </div>
+    </div>
+    <div className="frame__container frame__container--center">
+      <V8chLogo />
+    </div>
+    <div className="frame__container frame__container--bottom">
+      <div className="split-header split-header--light">
+        <h4>
+          Favor creation
+        </h4>
+        <h5>
+          Over consumption
+        </h5>
+      </div>
+    </div>
+  </div>
+);
 
 export default LandingHero;
