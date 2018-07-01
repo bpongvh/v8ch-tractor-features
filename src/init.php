dd73090b0cf7c1e5e3d57890b9765da1bfde1402
Robert Pratt <bpong@v8ch.com>
2018-07-01 17:02:40 +0700

    Rename assets

--- src/init.php
+++ src/init.php
@@ -17,30 +17,45 @@ add_action(
     'init',
     function () {
         wp_register_script(
-            'v8ch/block-v8ch-hero-js',
-            plugins_url('/dist/scripts/v8ch-hero.js', dirname(__FILE__)),
+            'v8ch/block-v8ch-hero',
+            plugins_url('/dist/scripts/block-v8ch-hero.js', dirname(__FILE__)),
             array( 'wp-blocks', 'wp-element')
         );
         wp_register_script(
-            'v8ch/block-v8ch-skills-js',
-            plugins_url('/dist/scripts/v8ch-skills.js', dirname(__FILE__)),
+            'v8ch/block-v8ch-projects',
+            plugins_url('/dist/scripts/block-v8ch-projects.js', dirname(__FILE__)),
+            array( 'wp-blocks', 'wp-element')
+        );
+        wp_register_script(
+            'v8ch/block-v8ch-skills',
+            plugins_url('/dist/scripts/block-v8ch-skills.js', dirname(__FILE__)),
             array( 'wp-blocks', 'wp-element')
         );
         wp_register_style(
-            'v8ch/block-v8ch-hero-css',
-            plugins_url('/dist/styles/v8ch-hero.css', dirname(__FILE__)),
+            'v8ch/tractor-blocks',
+            plugins_url('/dist/styles/tractor-blocks.css', dirname(__FILE__)),
             array( 'wp-edit-blocks' )
         );
         register_block_type(
             'v8ch/v8ch-hero',
             array(
-            'editor_script' => 'v8ch/block-v8ch-hero-js',
-            'editor_style'  => 'v8ch/block-v8ch-hero-css'
+            'editor_script' => 'v8ch/block-v8ch-hero',
+            'editor_style'  => 'v8ch/tractor-blocks'
+            )
+        );
+        register_block_type(
+            'v8ch/v8ch-projects',
+            array(
+            'editor_script' => 'v8ch/block-v8ch-projects',
+            'editor_style'  => 'v8ch/tractor-blocks'
             )
         );
         register_block_type(
             'v8ch/v8ch-skills',
-            array('editor_script' => 'v8ch/block-v8ch-skills-js')
+            array(
+            'editor_script' => 'v8ch/block-v8ch-skills',
+            'editor_style'  => 'v8ch/tractor-blocks'
+            )
         );
     },
     100
