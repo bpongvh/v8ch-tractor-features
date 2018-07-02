8bd8c2d73c678d704c4f13dd98a5e7c1a5279481
Robert Pratt <bpong@v8ch.com>
2018-07-02 13:52:28 +0700

    Initial commit

---
+++ src/scripts/app-v8ch-hero.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LandingHero from './components/LandingHero';
+
+ReactDOM.render(
+  <LandingHero />,
+  document.getElementById('v8ch-hero-mount'),
+);
