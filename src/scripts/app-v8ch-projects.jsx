567e426d78693c429f08c105211dfb78e456f351
Robert Pratt <bpong@v8ch.com>
2018-07-03 07:50:33 +0700

    Initial commit

---
+++ src/scripts/app-v8ch-projects.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProjectsList from './components/ProjectsList';
+
+ReactDOM.render(
+  <ProjectsList />,
+  document.getElementById('v8ch-projects-mount'),
+);
