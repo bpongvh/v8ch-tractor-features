487de7e563b4ec4af00d373efbba64bf9597a7cf
Robert Pratt <bpong@v8ch.com>
2018-07-05 09:34:49 +0700

    Pass projects data

--- src/scripts/app-v8ch-projects.jsx
+++ src/scripts/app-v8ch-projects.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ProjectsList from './components/ProjectsList';
 
+const mount = document.getElementById('v8ch-projects-mount');
+
 ReactDOM.render(
-  <ProjectsList />,
-  document.getElementById('v8ch-projects-mount'),
+  <ProjectsList projects={JSON.parse(mount.dataset.projects)} />,
+  mount,
 );
