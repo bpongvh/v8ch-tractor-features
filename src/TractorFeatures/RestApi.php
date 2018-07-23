d813d85b8f66fcd0d3a5e264b41338b4016178c5
Robert Pratt <bpong@v8ch.com>
2018-07-23 15:30:26 +0700

    Refactor, adding only for post type featured_link

--- src/TractorFeatures/RestApi.php
+++ src/TractorFeatures/RestApi.php
@@ -17,43 +17,20 @@ class RestApi
 
     public function addBlocks()
     {
-        $post_types = get_post_types_by_support(['editor']);
-        foreach ($post_types as $post_type) {
-            if (gutenberg_can_edit_post_type($post_type)) {
-                register_rest_field(
-                    $post_type,
-                    'data',
-                    [
-                        'get_callback' => [$this, 'getBlockData'],
-                    ]
-                );
-            }
-        }
-    }
-
-    public function getBlockData(array $post)
-    {
-        $blocks = gutenberg_parse_blocks($post['content']['raw']);
-        $processed= array_reduce(
-            $blocks,
-            function ($carry, $block) {
-                switch ($block['blockName']) {
-                    case 'v8ch/featured-link':
-                        $formatted = $this->formatFeaturedLink($block);
-                        array_push($carry, $formatted);
-                        return $carry;
-                    default:
-                        return $carry;
-                }
-            },
-            []
+        register_rest_field(
+            'featured_link',
+            'data',
+            [
+                'get_callback' => [$this, 'formatFeaturedLink'],
+            ]
         );
-
-        return $processed;
     }
 
-    public function formatFeaturedLink($block)
+    public function formatFeaturedLink(array $post)
     {
+        $parsed = gutenberg_parse_blocks($post['content']['raw']);
+        $block = array_shift($parsed);
+
         return [
             'description' => $block['attrs']['description'],
             'href' => $block['attrs']['href'],
